c928eb8487d46db4f748e19089226a4726ca6781
高志鹏 <104517120@qq.com>
2020-04-08 15:12:10 +0800

    feat: tree of algorithm & blog

--- docs/.vuepress/constants/jsonTree/blog.js
+++ docs/.vuepress/constants/jsonTree/blog.js
@@ -1,248 +1,258 @@
 export default {
-    id: 'Blog',
-    level: 0,
-    children: [
+  id: "Blog",
+  level: 0,
+  children: [
+    {
+      id: "Daily Question",
+      path: "/blog/@dailyQ.html",
+      level: 1,
+      children: [
         {
-            id: 'Daily Question',
-            path: '/blog/@dailyQ.html',
-            level: 1,
-            children: [
-                {
-                    id: 'React/Vue列表渲染key的作用',
-                    path: '/blog/@dailyQ.html#_1-react-vue列表渲染key的作用？',
-                    level: 2,
-                },
-                {
-                    id: 'ES5/ES6继承除写法以外的区别',
-                    path: '/blog/@dailyQ.html#_2-es5-es6继承除写法以外的区别？',
-                    level: 2,
-                },
-                {
-                    id: '简单实现一个eval',
-                    path: '/blog/@dailyQ.html#_3-简单实现一个eval',
-                    level: 2,
-                },
-                {
-                    id: 'parseInt深入',
-                    path: '/blog/@dailyQ.html#_4-parseint深入',
-                    level: 2,
-                },
-                {
-                    id: 'position sticky',
-                    path: '/blog/@dailyQ.html#_5-position-sticky',
-                    level: 2,
-                }
-            ]
+          id: "React/Vue列表渲染key的作用",
+          path: "/blog/@dailyQ.html#_1-react-vue列表渲染key的作用？",
+          level: 2,
         },
         {
-            id: 'Works Solution',
-            path: '/blog/@workSolutions.html',
-            level: 1,
-            children: [
-                {
-                    id: 'IE中打开谷歌',
-                    path: '/blog/@workSolutions.html#ie中打开谷歌',
-                    level: 2,
-                },
-                {
-                    id: '小程序textarea输入行数限制',
-                    path: '/blog/@workSolutions.html#小程序textarea输入行数限制',
-                    level: 2,
-                }
-            ]
+          id: "ES5/ES6继承除写法以外的区别",
+          path: "/blog/@dailyQ.html#_2-es5-es6继承除写法以外的区别？",
+          level: 2,
         },
         {
-            id: '十大经典排序',
-            level: 1,
-            path: '/blog/basicSort.html',
-            children: [
-                {
-                    id: '冒泡',
-                    path: '/blog/basicSort.html#冒泡排序',
-                    level: 2,
-                },
-                {
-                    id: '选择',
-                    path: '/blog/basicSort.html#选择排序',
-                    level: 2,
-                },
-                {
-                    id: '插入',
-                    path: '/blog/basicSort.html#插入排序',
-                    level: 2,
-                },
-                {
-                    id: '希尔',
-                    path: '/blog/basicSort.html#希尔排序',
-                    level: 2,
-                },
-                {
-                    id: '归并',
-                    path: '/blog/basicSort.html#归并排序',
-                    level: 2,
-                },
-                {
-                    id: '单路/双路快排',
-                    path: '/blog/basicSort.html#单路-双路快排',
-                    level: 2,
-                },
-                {
-                    id: '三路快排',
-                    path: '/blog/basicSort.html#三路快排',
-                    level: 2,
-                },
-                {
-                    id: '堆排序',
-                    path: '/blog/basicSort.html#堆排序',
-                    level: 2,
-                },
-                {
-                    id: '计数排序',
-                    path: '/blog/basicSort.html#计数排序',
-                    level: 2,
-                },
-                {
-                    id: '桶排序',
-                    path: '/blog/basicSort.html#桶排序',
-                    level: 2,
-                },
-                {
-                    id: '基数排序',
-                    path: '/blog/basicSort.html#基数排序',
-                    level: 2,
-                }
-            ]
+          id: "简单实现一个eval",
+          path: "/blog/@dailyQ.html#_3-简单实现一个eval",
+          level: 2,
         },
         {
-            id: '中央定时器',
-            path: '/blog/centralTimer.html',
-            level: 1,
+          id: "parseInt深入",
+          path: "/blog/@dailyQ.html#_4-parseint深入",
+          level: 2,
         },
         {
-            id: 'vue magic modal',
-            path: '/blog/magicModal.html',
-            level: 1,
+          id: "position sticky",
+          path: "/blog/@dailyQ.html#_5-position-sticky",
+          level: 2,
         },
         {
-            id: '大顶堆实现',
-            path: '/blog/maxHeap.html',
-            level: 1,
+          id: "call&apply&bind",
+          path: "/blog/@dailyQ.html#_6-call-apply-bind-实现",
+          level: 2,
+        },
+      ],
+    },
+    {
+      id: "Works Solution",
+      path: "/blog/@workSolutions.html",
+      level: 1,
+      children: [
+        {
+          id: "IE中打开谷歌",
+          path: "/blog/@workSolutions.html#ie中打开谷歌",
+          level: 2,
         },
         {
-            id: 'reduce实现',
-            path: '/blog/reduce.html',
-            level: 1,
+          id: "小程序textarea输入行数限制",
+          path: "/blog/@workSolutions.html#小程序textarea输入行数限制",
+          level: 2,
+        },
+      ],
+    },
+    {
+      id: "十大经典排序",
+      level: 1,
+      path: "/blog/basicSort.html",
+      children: [
+        {
+          id: "冒泡",
+          path: "/blog/basicSort.html#冒泡排序",
+          level: 2,
         },
         {
-            id: '正则',
-            path: '/blog/regexp.html',
-            level: 1,
-            children: [
-                {
-                    id: '创建',
-                    path: '/blog/regexp.html#创建',
-                    level: 2,
-                },
-                {
-                    id: '方法',
-                    path: '/blog/regexp.html#方法',
-                    level: 2,
-                },
-                {
-                    id: '属性',
-                    path: '/blog/regexp.html#实例属性和静态属性',
-                    level: 2,
-                },
-                {
-                    id: 'Sticky',
-                    path: '/blog/regexp.html#sticky-y',
-                    level: 2,
-                },
-                {
-                    id: 'Unicode',
-                    path: '/blog/regexp.html#unicode-u',
-                    level: 2,
-                },
-                {
-                    id: '新特性补充',
-                    path: '/blog/regexp.html#新特性补充',
-                    level: 2,
-                }
-            ]
+          id: "选择",
+          path: "/blog/basicSort.html#选择排序",
+          level: 2,
         },
         {
-            id: 'Vue2.x原理浅析',
-            path: '/blog/vuerealize.html',
-            level: 1,
+          id: "插入",
+          path: "/blog/basicSort.html#插入排序",
+          level: 2,
         },
         {
-            id: 'Promise实现原理',
-            path: '/blog/promiseRealize.html',
-            level: 1,
+          id: "希尔",
+          path: "/blog/basicSort.html#希尔排序",
+          level: 2,
         },
         {
-            id: 'Css之Grid',
-            path: '/blog/cssGrid.html',
-            level: 1,
-            children: [
-                {
-                    id: '网格容器(Grid Container)',
-                    path: '/blog/cssGrid.html#网格容器-grid-container',
-                    level: 2,
-                },
-                {
-                    id: '网格项(Grid Items)',
-                    path: '/blog/cssGrid.html#网格项-grid-items',
-                    level: 2,
-                },
-            ]
+          id: "归并",
+          path: "/blog/basicSort.html#归并排序",
+          level: 2,
         },
         {
-            id: '深入深拷贝',
-            path: '/blog/deepClone.html',
-            level: 1,
-            children: [
-                {
-                    id: 'JSON',
-                    path: '/blog/deepClone.html#json方法',
-                    level: 2,
-                },
-                {
-                    id: 'Object.assign',
-                    path: '/blog/deepClone.html#使用object-assign',
-                    level: 2,
-                },
-                {
-                    id: 'Messagechannel',
-                    path: '/blog/deepClone.html#使用messagechannel',
-                    level: 2,
-                },
-                {
-                    id: '深度优先',
-                    path: '/blog/deepClone.html#深度优先',
-                    level: 2,
-                },
-                {
-                    id: '广度优先',
-                    path: '/blog/deepClone.html#广度优先',
-                    level: 2,
-                },
-                {
-                    id: '_.cloneDeep',
-                    path: '/blog/deepClone.html#clonedeep-lodash-4-17-15',
-                    level: 2,
-                },
-            ]
+          id: "单路/双路快排",
+          path: "/blog/basicSort.html#单路-双路快排",
+          level: 2,
         },
         {
-            id: 'Async',
-            path: '/blog/async.html',
-            level: 1,
+          id: "三路快排",
+          path: "/blog/basicSort.html#三路快排",
+          level: 2,
         },
         {
-            id: '函数柯里化',
-            path: '/blog/curryFunc.html',
-            level: 1,
+          id: "堆排序",
+          path: "/blog/basicSort.html#堆排序",
+          level: 2,
         },
-    ]
+        {
+          id: "计数排序",
+          path: "/blog/basicSort.html#计数排序",
+          level: 2,
+        },
+        {
+          id: "桶排序",
+          path: "/blog/basicSort.html#桶排序",
+          level: 2,
+        },
+        {
+          id: "基数排序",
+          path: "/blog/basicSort.html#基数排序",
+          level: 2,
+        },
+      ],
+    },
+    {
+      id: "中央定时器",
+      path: "/blog/centralTimer.html",
+      level: 1,
+    },
+    {
+      id: "vue magic modal",
+      path: "/blog/magicModal.html",
+      level: 1,
+    },
+    {
+      id: "大顶堆实现",
+      path: "/blog/maxHeap.html",
+      level: 1,
+    },
+    {
+      id: "reduce实现",
+      path: "/blog/reduce.html",
+      level: 1,
+    },
+    {
+      id: "正则",
+      path: "/blog/regexp.html",
+      level: 1,
+      children: [
+        {
+          id: "创建",
+          path: "/blog/regexp.html#创建",
+          level: 2,
+        },
+        {
+          id: "方法",
+          path: "/blog/regexp.html#方法",
+          level: 2,
+        },
+        {
+          id: "属性",
+          path: "/blog/regexp.html#实例属性和静态属性",
+          level: 2,
+        },
+        {
+          id: "Sticky",
+          path: "/blog/regexp.html#sticky-y",
+          level: 2,
+        },
+        {
+          id: "Unicode",
+          path: "/blog/regexp.html#unicode-u",
+          level: 2,
+        },
+        {
+          id: "新特性补充",
+          path: "/blog/regexp.html#新特性补充",
+          level: 2,
+        },
+      ],
+    },
+    {
+      id: "Vue2.x原理浅析",
+      path: "/blog/vuerealize.html",
+      level: 1,
+    },
+    {
+      id: "Promise实现原理",
+      path: "/blog/promiseRealize.html",
+      level: 1,
+    },
+    {
+      id: "Css之Grid",
+      path: "/blog/cssGrid.html",
+      level: 1,
+      children: [
+        {
+          id: "网格容器(Grid Container)",
+          path: "/blog/cssGrid.html#网格容器-grid-container",
+          level: 2,
+        },
+        {
+          id: "网格项(Grid Items)",
+          path: "/blog/cssGrid.html#网格项-grid-items",
+          level: 2,
+        },
+      ],
+    },
+    {
+      id: "深入深拷贝",
+      path: "/blog/deepClone.html",
+      level: 1,
+      children: [
+        {
+          id: "JSON",
+          path: "/blog/deepClone.html#json方法",
+          level: 2,
+        },
+        {
+          id: "Object.assign",
+          path: "/blog/deepClone.html#使用object-assign",
+          level: 2,
+        },
+        {
+          id: "Messagechannel",
+          path: "/blog/deepClone.html#使用messagechannel",
+          level: 2,
+        },
+        {
+          id: "深度优先",
+          path: "/blog/deepClone.html#深度优先",
+          level: 2,
+        },
+        {
+          id: "广度优先",
+          path: "/blog/deepClone.html#广度优先",
+          level: 2,
+        },
+        {
+          id: "_.cloneDeep",
+          path: "/blog/deepClone.html#clonedeep-lodash-4-17-15",
+          level: 2,
+        },
+      ],
+    },
+    {
+      id: "Async",
+      path: "/blog/async.html",
+      level: 1,
+    },
+    {
+      id: "函数柯里化",
+      path: "/blog/curryFunc.html",
+      level: 1,
+    },
+    {
+      id: "防抖和节流",
+      path: "/blog/Debounce&Throttle.html",
+      level: 1,
+    },
+  ],
 }
