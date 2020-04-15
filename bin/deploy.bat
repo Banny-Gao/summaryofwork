ec31f6645859c6d069f73538ed14b22a2cb88c0b
高志鹏 <104517120@qq.com>
2020-04-15 22:49:51 +0800

    chore: fix dependencies version

--- bin/deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off 
-cd dist/
-git init 
-git config core.autocrlf false
-git remote add origin git@github.com:Mackkkk/SummaryOfwork.git
-git add .
-git commit -m 'publish'
-git pull origin master
-git push origin master
