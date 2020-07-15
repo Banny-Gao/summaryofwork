fff76a592d86b2cb033d8a0d08365e875f6ccdfb
高志鹏 <104517120@qq.com>
2020-07-15 10:29:23 +0800

    feat: add pwa

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,10 +10,13 @@ module.exports = {
   title: 'Summary Of work',
   description: 'The description of the site.',
   head: [
-    ['link', {
-      rel: 'icon',
-      href: `/logo.png`
-    }]
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: '#3eaf7c' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
   ],
   base: '/',
   dest: './dist',
@@ -70,7 +73,10 @@ module.exports = {
       md.use(require('markdown-it-katex'))
     }
   },
-  plugins: ['@vuepress/back-to-top'],
+  plugins: [
+    '@vuepress/back-to-top',
+    '@vuepress/pwa',
+  ],
   chainWebpack (config) {
     // config 是一个 ChainableConfig 的实例
     config.plugin('analyzer').use(BundleAnalyzerPlugin)
